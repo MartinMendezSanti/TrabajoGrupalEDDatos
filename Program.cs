b2e13a3f1e85d16a10fd0acde882e6881a0e6fb0
martinmendez <martin.a.mendez91@gmail.com>
2020-10-23 22:10:04 -0300

    se agregaron las funciones desarrolladas por Nico

--- Program.cs
+++ Program.cs
@@ -7,9 +7,74 @@ using System.Collections;
 
 namespace trabajogrupalEDDatos
 {
+    //Clase alumno
+    public class Alumno
+    {
+        public string Name { get; set; }
+        public int Nota1 { get; set; }
+        public int Nota2 { get; set; }
+        public int Nota3 { get; set; }
+        public string Lastname { get; set; }
+
+        public override string ToString()
+        {
+            return "Alumno: " + Name + " " + Lastname + " - Notas: " + Nota1 + " / " + Nota2 + " / " + Nota3;
+        }
+    }
     class Program
     {
-        public static string IngresoUsuario(Hashtable diccionario)
+        
+        public static void PromediarAlumno(List<Alumno> listado)
+        {
+            for (int i = 0; i < listado.Count; i++)
+            {
+                Console.Write(listado[i].Name + " " + listado[i].Lastname);
+                float promedio = (listado[i].Nota1 + listado[i].Nota2 + listado[i].Nota3) / 3;
+
+                if (promedio != 0)
+                {
+                    Console.Write(" tiene de promedio: " + promedio + "\n");
+                }
+                else
+                {
+                    Console.Write(" : No tiene notas cargadas. \n");
+                }
+            }
+        }
+        public static void BuscarAlumno(List<Alumno> lista)
+        {
+            //Buscar alumno
+            Console.Write("\nSi desea buscar alumno escriba su nombre: ");
+            string buscado = Console.ReadLine();
+            Console.WriteLine("\nDatos del alumno: {0}\n", lista.Find(x => x.Name.Contains(buscado)));
+        }
+        public static List<Alumno> IngresarAlumnos()
+        {
+            //Lista de alumnos
+            List<Alumno> listaDeAlumnos = new List<Alumno>();
+
+            //Flag para terminar la carga
+            string salir = "no";
+
+
+            //Carga de datos
+            do
+            {
+                Console.Write("\nIngrese nombre alumno: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Ingrese apellido del alumno: ");
+                string apellido = Console.ReadLine();
+
+                listaDeAlumnos.Add(new Alumno() { Name = nombre, Lastname = apellido });
+                Console.Write("\n> ¿Desea agregar más alumnos? si/no  ");
+                salir = Console.ReadLine();
+
+            }
+            while (salir != "no");
+
+            return listaDeAlumnos;
+        }
+            public static string IngresoUsuario(Hashtable diccionario)
         {
             string usuario, pass;
             int contador = 0;
@@ -52,6 +117,60 @@ namespace trabajogrupalEDDatos
 
         }
 
+        public static int validador()
+        {
+            int valor;
+            string num;
+            bool esNumero;
+            do
+            {
+                Console.Write("Ingrese nota entre 1 y 10: ");
+                num = Console.ReadLine();
+                /* Si es número correcto retornará true y saldrá del Ciclo*/
+                esNumero = int.TryParse(num, out valor);
+
+                while (valor > 10 || valor < 0)
+                {
+                    Console.Write("Numero incorrecta, ingrese nota entre 1 y 10: ");
+                    num = Console.ReadLine();
+                    esNumero = int.TryParse(num, out valor);
+                }
+                if (!esNumero) Console.Write("Nota incorrecta.");
+            }
+            while (!esNumero);
+
+            return valor;
+        }
+        public static List<Alumno> IngresarNotas(ref List<Alumno> listado)
+        {
+            //Agregar Nota a algun alumno en particular
+            string salir;
+            do
+            {
+                Console.Write("\nEscriba el nombre del alumno que desea agregar las notas: ");
+                string buscarAlumno = Console.ReadLine();
+
+                //Listado de alumnos con foreach
+                foreach (Alumno lista in listado)
+                {
+                    if (lista.Name == buscarAlumno)
+                    {
+                        Console.WriteLine("\nIngresar a continuacion las 3 notas del cuatrimestre.\n");
+                        int notauno = validador();
+                        int notados = validador();
+                        int notatres = validador();
+                        lista.Nota1 = notauno; lista.Nota2 = notados; lista.Nota3 = notatres;
+                    }
+
+                }
+                Console.Write("\n ¿Desea agregar más notas? si/no ");
+                salir = Console.ReadLine();
+            }
+            while (salir != "no");
+
+            return listado;
+
+        }
         public static ArrayList GenerarUsuario()
         {
             //Se le pide al usuario ingresar un usuario, una contraseña y volver a ingresar la contraseña.
@@ -86,6 +205,7 @@ namespace trabajogrupalEDDatos
         }
         static void Main(string[] args)
         {
+            List<Alumno> listadoDeAlumnos = new List<Alumno>();
             int digito, opcion_elegida;
             string nombre_usuario, password;
             //Como no tenemos BD usamos un diccionario con el par usuario/clave.
@@ -128,23 +248,51 @@ namespace trabajogrupalEDDatos
             Console.WriteLine("Porfavor ingrese el número de operación que desea realizar.");
             Console.WriteLine("Ingresar datos de un alumno: 1");
             Console.WriteLine("Ingresar notas: 2");
-            Console.WriteLine("Calcular Promedio de un alumno: 3");
-            Console.WriteLine("Calcular Promedio total del curso: 4");
-            Console.WriteLine("Salir: 5");
+            Console.WriteLine("Ingresar notas: 3");
+            Console.WriteLine("Calcular Promedio de un alumno: 4");
+            Console.WriteLine("Calcular Promedio total del curso: 5");
+            Console.WriteLine("Ver la cantidad de alumnos: 6");
+            Console.WriteLine("Salir: 7");
             opcion_elegida = Convert.ToInt32(Console.ReadLine());
 
             switch (opcion_elegida)
             {
-                case "1": IngresarAlumnos();
+                case 1:
+                    {
+                        listadoDeAlumnos = IngresarAlumnos();
                         break;
-                case "2": IngresarNotas();
+                    }
+                case 2:
+                    {
+                        IngresarNotas(ref listadoDeAlumnos);
                         break;
-                case "3": PromediarAlumno();
+                    }
+                case 3:
+                    {
+                        BuscarAlumno(listadoDeAlumnos);
                         break;
-                case "4": PromediarCurso();
+                    }
+                case 4:
+                    {
+                        PromediarAlumno(listadoDeAlumnos);
                         break;
-                case "5": Console.WriteLine("Gracias por usar el gestor de alumnos y cursadas");
+                    }
+                case 5:
+                    {
+                        PromediarCurso();
                         break;
+                    }
+                case 6: 
+                    { 
+                        Console.WriteLine("\nEste curso tiene " + listadoDeAlumnos.Count + " alumnos.\n");
+                        break;
+                    }
+
+                case 7:
+                    {
+                        Console.WriteLine("Gracias por usar el gestor de alumnos y cursadas");
+                        break;
+                    }
             }
 
         }
