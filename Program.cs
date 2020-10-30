b1acc196b29635e8dce21069e9417cb00f7cef53
Nico Mirabello <nsmirabello@gmail.com>
2020-10-29 22:32:19 -0300

    Subo la version del del Tp

--- Program.cs
+++ Program.cs
@@ -18,12 +18,12 @@ namespace trabajogrupalEDDatos
 
         public override string ToString()
         {
-            return "Alumno: " + Name + " " + Lastname + " - Notas: " + Nota1 + " / " + Nota2 + " / " + Nota3;
+            return "Alumno: " + Name + " " + Lastname + " / 1er. Nota: " + Nota1 + " - 2da. Nota: " + Nota2 + " - 3er. Nota: " + Nota3;
         }
     }
     class Program
     {
-        
+
         public static void PromediarAlumno(List<Alumno> listado)
         {
             for (int i = 0; i < listado.Count; i++)
@@ -74,24 +74,24 @@ namespace trabajogrupalEDDatos
 
             return listaDeAlumnos;
         }
-            public static string IngresoUsuario(Hashtable diccionario)
+        public static string IngresoUsuario(Hashtable diccionario)
         {
             string usuario, pass;
             int contador = 0;
-            
+
             //Pedimos que ingrese su usuario y contraseña
             Console.WriteLine("Ingrese su usuario:");
             usuario = Console.ReadLine();
             Console.WriteLine("Ingrese su contraseña:");
             pass = Console.ReadLine();
-            
+
             //Vamos a enumerar el diccionario donde tenemos guardados los usuarios
             //para poder ir comparando si el usuario y contraseña ingresados se encuentran
             //en el diccionario; si es así se agrega un 1 al contador.
             IDictionaryEnumerator miEnumerador = diccionario.GetEnumerator();
             while (miEnumerador.MoveNext())
             {
-                if (usuario == (string) miEnumerador.Key && pass == (string) miEnumerador.Value)
+                if (usuario == (string)miEnumerador.Key && pass == (string)miEnumerador.Value)
                     contador++;
             }
 
@@ -174,34 +174,34 @@ namespace trabajogrupalEDDatos
         public static ArrayList GenerarUsuario()
         {
             //Se le pide al usuario ingresar un usuario, una contraseña y volver a ingresar la contraseña.
-            
+
             string nombre_elegido, pass1, pass2;
-            Console.WriteLine("Bienvenido al generador de usuario.");
-            Console.WriteLine("Ingrese el nombre de usuario:");
+            Console.WriteLine("\nBienvenido al generador de usuario.");
+            Console.Write("\tIngrese el nombre de usuario: ");
             nombre_elegido = Console.ReadLine();
-            Console.WriteLine("Ingrese la contraseña:");
+            Console.Write("\tIngrese la contraseña: ");
             pass1 = Console.ReadLine();
-            Console.WriteLine("Ingrese nuevamente la contraseña:");
+            Console.Write("\tIngrese nuevamente la contraseña:");
             pass2 = Console.ReadLine();
 
             //Valida que la contraseña ingresada las dos veces coincida.
             while (pass1 != pass2)
             {
-                Console.WriteLine("Las contraseñas no coincide. Intente nuevamente");
-                Console.WriteLine("Ingrese la contraseña:");
+                Console.WriteLine("\nLas contraseñas no coincide. Intente nuevamente.\n");
+                Console.Write("\tIngrese la contraseña: ");
                 pass1 = Console.ReadLine();
-                Console.WriteLine("Ingrese nuevamente la contraseña:");
+                Console.Write("\tIngrese nuevamente la contraseña: ");
                 pass2 = Console.ReadLine();
             }
 
             //Crea una lista donde guarda los valores del nombre de usuario y contraseña para devolver eso.
-            Console.WriteLine("Usuario y contraseña generado exitosamente.");
+            Console.WriteLine("Usuario y contraseña generado exitosamente.\n");
             ArrayList listaUsuario = new ArrayList();
             listaUsuario.Add(nombre_elegido);
             listaUsuario.Add(pass1);
 
             return listaUsuario;
-            
+
         }
         static void Main(string[] args)
         {
@@ -213,87 +213,111 @@ namespace trabajogrupalEDDatos
             diccioUsuarios.Add("Eduardo", "pass1");
             diccioUsuarios.Add("Mariela", "pass2");
             diccioUsuarios.Add("Pablo", "pass3");
-            
-            Console.WriteLine("Bienvenido al gestor de alumnos y cursadas");
-            Console.WriteLine("Si ya posee un usuario ingrese 1; si desea generar un usuario ingrese 2");
+
+            Console.WriteLine("Bienvenido al gestor de alumnos y cursadas.\n");
+            Console.WriteLine("\t1 - Si ya posee un usuario.\n\t2 - Si desea generar un usuario.");
+            Console.Write("\t> Ingrese opción: ");
             digito = Convert.ToInt32(Console.ReadLine());
 
             //Valida que se haya ingresado 1 o 2
             while (digito != 1 && digito != 2)
             {
-                Console.WriteLine("Ingresó un caracter incorrecto. Intente nuevamente");
-                Console.WriteLine("Si ya posee un usuario ingrese 1; si desea generar un usuario ingrese 2");
+                Console.WriteLine("Ingresó un caracter incorrecto. Intente nuevamente.\n");
+                Console.WriteLine("\t1 - Si ya posee un usuario.\n\t2 - Si desea generar un usuario.");
+                Console.Write("\t> Ingrese opción: ");
                 digito = Convert.ToInt32(Console.ReadLine());
             }
 
             if (digito == 1)
             {
                 nombre_usuario = IngresoUsuario(diccioUsuarios);
-                Console.WriteLine("Bienvenido {0}", nombre_usuario);
+                Console.WriteLine("\nBienvenido {0}\n", nombre_usuario);
             }
             else if (digito == 2)
-            { 
-                
+            {
+
                 //Llama a la función de generar usuario, que le va a devolver una lista.
                 //Guarda los elementos de esa lista en variables string y los suma al
                 //diccionario de usuario/clave.
                 ArrayList lista_user = GenerarUsuario();
-                nombre_usuario = (string) lista_user[0];
-                password = (string) lista_user[1];
+                nombre_usuario = (string)lista_user[0];
+                password = (string)lista_user[1];
                 diccioUsuarios.Add(nombre_usuario, password);
-                Console.WriteLine("Bienvenido {0}", nombre_usuario);
+                Console.WriteLine("\nBienvenido {0}\n", nombre_usuario);
 
             }
 
-            Console.WriteLine("Porfavor ingrese el número de operación que desea realizar.");
-            Console.WriteLine("Ingresar datos de un alumno: 1");
-            Console.WriteLine("Ingresar notas: 2");
-            Console.WriteLine("Ingresar notas: 3");
-            Console.WriteLine("Calcular Promedio de un alumno: 4");
-            Console.WriteLine("Calcular Promedio total del curso: 5");
-            Console.WriteLine("Ver la cantidad de alumnos: 6");
-            Console.WriteLine("Salir: 7");
-            opcion_elegida = Convert.ToInt32(Console.ReadLine());
-
-            switch (opcion_elegida)
+    
+           
+            do
             {
-                case 1:
-                    {
-                        listadoDeAlumnos = IngresarAlumnos();
-                        break;
-                    }
-                case 2:
-                    {
-                        IngresarNotas(ref listadoDeAlumnos);
-                        break;
-                    }
-                case 3:
-                    {
-                        BuscarAlumno(listadoDeAlumnos);
-                        break;
-                    }
-                case 4:
-                    {
-                        PromediarAlumno(listadoDeAlumnos);
-                        break;
-                    }
-                case 5:
-                    {
-                        PromediarCurso();
-                        break;
-                    }
-                case 6: 
-                    { 
-                        Console.WriteLine("\nEste curso tiene " + listadoDeAlumnos.Count + " alumnos.\n");
-                        break;
-                    }
+                Console.WriteLine("Por favor ingrese la opción que desea realizar.\n");
+                Console.WriteLine("\t> 1 - Ingresar nombre y apellido de alumnos.");
+                Console.WriteLine("\t> 2 - Ingresar notas de alumnos.");
+                Console.WriteLine("\t> 3 - Buscar la información de un alumno.");
+                Console.WriteLine("\t> 4 - Ver los promedios de los alumnos.");
+                Console.WriteLine("\t> 5 - Calcular Promedio total del curso.");
+                Console.WriteLine("\t> 6 - Ver la cantidad de alumnos que tiene el curso.");
+                Console.WriteLine("\t> 7 - Salir.");
+                Console.Write("\n\t\tIngrese la opción: ");
+                opcion_elegida = Convert.ToInt32(Console.ReadLine());
 
-                case 7:
-                    {
-                        Console.WriteLine("Gracias por usar el gestor de alumnos y cursadas");
-                        break;
-                    }
+
+                switch (opcion_elegida)
+                {
+                    case 1:
+                        {
+                            listadoDeAlumnos = IngresarAlumnos();
+                            break;
+                        }
+                    case 2:
+                        {
+                            IngresarNotas(ref listadoDeAlumnos);
+                            break;
+                        }
+                    case 3:
+                        {
+                            BuscarAlumno(listadoDeAlumnos);
+                            break;
+                        }
+                    case 4:
+                        {
+                            PromediarAlumno(listadoDeAlumnos);
+                            break;
+                        }
+                    /*case 5:
+                        {
+                            PromediarCurso();
+                            break;
+                        }*/
+                    case 6:
+                        {
+                            Console.WriteLine("\nEste curso tiene " + listadoDeAlumnos.Count + " alumnos.\n");
+                            break;
+                        }
+
+                    case 7:
+                        {
+                            Console.WriteLine("\nGracias por usar el gestor de alumnos y cursadas");
+                            break;
+                        }
+                }
+               
+               
+                if (opcion_elegida == 7)
+                {
+                    Console.WriteLine("\n Sesión finalizada correctamente.");
+                }
+                else
+                {
+                    Console.WriteLine("\nPulse cualquier tecla para volver al menu.\n");
+                }
+                Console.ReadKey();
+
+                /*Console.Clear();*/
+
             }
+            while (opcion_elegida != 7);
 
         }
     }
